08bea6ecead82419342b0ebddf6584e3eea084ec
YutoMizutani <yuto.mizutani.dev@gmail.com>
2019-03-18 14:19:33 +0900

    Initial Commit

---
+++ SwiftInitGenerator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwiftInitGenerator
+//
+//  Created by ym on 2019/03/18.
+//  Copyright © 2019 Yuto Mizutani. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
